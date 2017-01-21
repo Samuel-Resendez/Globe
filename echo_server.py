6a45048826cd700c2cf2cf872347b95cef58a32d
Samuel Resendez <saresend@usc.edu>
2017-01-21 10:17:45 -0800

    setting up nice repo for mah boi Caleb

--- echo_server.py
+++ echo_server.py
@@ -1,26 +1,56 @@
 #Author: Samuel Resendez
 
 
+
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+import tornado.websocket as ws
+
+
+import eventlet
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question
 from geopy.geocoders import GoogleV3
+from flask_socketio import SocketIO, send, emit
+
 
 """ Setting Global Vars """
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app,async_mode='eventlet')
+
+
 ask = Ask(app, '/')
 geo_locator = GoogleV3(api_key="AIzaSyC0q7QFF35WNbYWvr4hdFrlsr6RUCGYLRw")
+data_sets = []
+user_id = 0
+
+
+### Initialization Functions ###
 
 @ask.launch
 def new_connection():
     """Called upon initialization"""
+    # r = requests.get(url)
+
+
     return question("Hello, welcome to Globe, what would you like to see today?")
 
 
 
+@app.route("/")
+def initialize():
+    return render_template('index.html',async_mode='eventlet')
+
 ### TRAVEL FUNCTIONS ###
+
+
+
 
 @ask.intent('goToDestinationIntent')
 def travel_to_location(location):
+    """Called to move to another location on the map. Should set up a websocket with react thingy"""
     # location is name of location
 
     print(location)
@@ -30,10 +60,9 @@ def travel_to_location(location):
     print(loc_obj.latitude, end=', ')
     print(loc_obj.longitude)
 
-    #do post request
-
+    # start websocket
+    send()
     text = render_template('viewLocation', latitute=int(loc_obj.latitude), longitude=int(loc_obj.longitude), location=location)
-
 
     return question(text)
 
@@ -65,6 +94,29 @@ def quit_map():
 
 
 
+### SOCKET POST FUNCTION ###
+
+@socketio.on('join', namespace='/sock')
+def join(message):
+
+    print("We are connected!")
+    print(message)
+
+
+
+
+### WEBSOCKET CLASSES ###
+class EchoWebSocket(ws.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+
 
 ### CONFIG FUNCTIONS ###
 
@@ -75,7 +127,8 @@ def hello_world():
 
 
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    print("Starting server...")
+    IOLoop.instance().start()
